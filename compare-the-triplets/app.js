e87433730d543d764204f7685e574a8a1b8d29a2
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2022-09-15 19:06:41 +0100

    hacker rank

---
+++ compare-the-triplets/app.js
@@ -0,0 +1,19 @@
+/*Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]). */
+
+function compareTriplets(a, b) {
+  let alice = 0;
+  let bob = 0
+
+  for(let i = 0; i < a.length; i++){
+    if(a[i] > b[i]) alice +=1
+    if(a[i] < b[i]) bob +=1
+  }
+
+  console.log([alice, bob])
+
+}
+
+
+compareTriplets([17, 28, 30], [99, 16, 8])
